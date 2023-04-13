63db12525614be4f8c5e9963a18a2bfddfe28d84
UmeshGolani <35754561+UmeshGolani@users.noreply.github.com>
2023-04-13 19:13:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function threeSum(arr, target) {
 // write your code here
-  
+  let binaryStr = ""
+while(num>0){
+let rem = num%2;
+binaryStr = binaryStr + rem.toString()
+num = Math.floor(num/2)
+}
+return binaryStr.split("").reverse().join("")
 }
 
 module.exports = threeSum;
